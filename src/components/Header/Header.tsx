c1a073d38ba449817a5ba04b414f063d3f8e0641
ALESBERESIK\beres <beresik.ales@gmail.com>
2022-09-19 21:22:02 +0200

    header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Header.css";
 import { FiChevronRight } from "react-icons/fi";
-import { AiOutlineArrowDown } from 'react-icons/ai'
-import {GoQuote} from 'react-icons/go'
-import justitia from '../assets/images/justitia.png'
-import hammer from '../assets/images/hammer.png'
+import { AiOutlineArrowDown } from "react-icons/ai";
+import { GoQuote } from "react-icons/go";
+import justitia from "../assets/images/justitia.png";
+import hammer from "../assets/images/hammer.png";
 
 function Header() {
   return (
@@ -21,29 +21,50 @@ function Header() {
       </div>
       <div className="header_info">
         <div className="header_info_stats">
-          <div className="projects_completed"></div>
-          <div className="break"/>
-          <div className="awards_gain"></div>
-          <div className="break"/>
-          <div className="branches"></div>
+          <div className="info">
+            <div className="projects_completed">
+              <h3>294</h3>
+              <h4>Projects completed</h4>
+            </div>
+            <div className="break" />
+            <div className="awards_gain">
+              <h3>25</h3>
+              <h4>Award again</h4>
+            </div>
+            <div className="break" />
+            <div className="branches">
+              <h3>10</h3>
+              <h4>Branches</h4>
+            </div>
+          </div>
         </div>
         <div className="header_info_image">
           <div className="header_info_image_background">
             <img src={justitia} alt="justitia" className="justitia" />
-              <AiOutlineArrowDown fontSize={30} className="arrow_down" href="/#about"/>
+            <AiOutlineArrowDown
+              fontSize={30}
+              className="arrow_down"
+              href="/#about"
+            />
           </div>
           <div className="header_info_image_arrow_down"></div>
         </div>
         <div className="header_info_text">
           <div className="hammer_img">
-            <img src={hammer} alt="hammer" className="header_info_text_hammer" />
+            <img
+              src={hammer}
+              alt="hammer"
+              className="header_info_text_hammer"
+            />
           </div>
           <div className="quote_div">
             <GoQuote fontSize={40} className="quote" />
-            </div>
+          </div>
           <div className="info_text">
             <p>We pritorise our</p>
-            <p>client's <span style={{color:'orangered'}}>statement</span></p>
+            <p>
+              client's <span style={{ color: "orangered" }}>statement</span>
+            </p>
           </div>
         </div>
         <div className="header_overlay"></div>
