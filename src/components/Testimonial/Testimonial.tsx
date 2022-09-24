d3206578adf8947c1b9747ca4098a866e3487cb7
ALESBERESIK\beres <beresik.ales@gmail.com>
2022-09-24 14:10:46 +0200

    terstimonial, contact form

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Message from "./Message";
 import "./Testimonial.css";
 
 function Testimonial() {
-  return <div>Testimonial</div>;
+  return (
+    <div className="testimonial">
+      <div className="testimonial_text">
+        <h2>Clients Testimonial</h2>
+      </div>
+      <div className="testimonial_client">
+        <Message />
+      </div>
+    </div>
+  );
 }
 
 export default Testimonial;
