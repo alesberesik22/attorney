4b8b5d2c93ffac14558b9d255f38af3149c0e70d
ALESBERESIK\beres <beresik.ales@gmail.com>
2022-09-24 21:25:14 +0200

    testimonial - need to finish animation

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Message from "./Message";
 import "./Testimonial.css";
+import testimonialData from "./Testimonialdata";
+import { motion } from "framer-motion";
 
 function Testimonial() {
   return (
@@ -8,9 +10,13 @@ function Testimonial() {
       <div className="testimonial_text">
         <h2>Clients Testimonial</h2>
       </div>
-      <div className="testimonial_client">
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 1 }}
+        className="testimonial_client"
+      >
         <Message />
-      </div>
+      </motion.div>
     </div>
   );
 }
---
+++ src/components/Testimonial/Testimonialdata.tsx
@@ -0,0 +1,40 @@
+import person from "../assets/images/person.jpg";
+const testimonialData = [
+  {
+    header: "Incredible experience",
+    text: "1Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus excepturi ullam, repellendus dolores eligendi voluptatum? A exofficiis rem mollitia porro labore consequuntur nesciunt? Blanditiis libero odit esse quibusdam?",
+    image: person,
+    name: "Ales Beresik",
+    position: "React Developer",
+  },
+  {
+    header: "Incredible experience",
+    text: "2Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus excepturi ullam, repellendus dolores eligendi voluptatum? A exofficiis rem mollitia porro labore consequuntur nesciunt? Blanditiis libero odit esse quibusdam?",
+    image: person,
+    name: "Ales Beresik",
+    position: "React Developer",
+  },
+  {
+    header: "Incredible experience",
+    text: "3Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus excepturi ullam, repellendus dolores eligendi voluptatum? A exofficiis rem mollitia porro labore consequuntur nesciunt? Blanditiis libero odit esse quibusdam?",
+    image: person,
+    name: "Ales Beresik",
+    position: "React Developer",
+  },
+  {
+    header: "Incredible experience",
+    text: "4Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus excepturi ullam, repellendus dolores eligendi voluptatum? A exofficiis rem mollitia porro labore consequuntur nesciunt? Blanditiis libero odit esse quibusdam?",
+    image: person,
+    name: "Ales Beresik",
+    position: "React Developer",
+  },
+  {
+    header: "Incredible experience",
+    text: "5Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus excepturi ullam, repellendus dolores eligendi voluptatum? A exofficiis rem mollitia porro labore consequuntur nesciunt? Blanditiis libero odit esse quibusdam?",
+    image: person,
+    name: "Ales Beresik",
+    position: "React Developer",
+  },
+];
+
+export default testimonialData;
